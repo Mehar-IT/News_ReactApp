3baac510a1ca52ed72b4a739adf65c3f84da1211
Mehar-It <hamzatariquearain@gmail.com>
2022-08-04 14:30:01 +0500

    refactored code

--- src/components/News.js
+++ src/components/News.js
@@ -22,95 +22,45 @@ export default class News extends Component {
       articles: [],
       loading: true,
       page: 1,
-      totalArticles: 0,
     };
   }
-  componentDidMount = async () => {
+
+  data = async () => {
+    this.setState({
+      loading: true,
+    });
     var options = {
       method: "GET",
       headers: {
         "x-api-key": "SFSG78-MkJhqh_pv5pgSB_aY5pVYHfuUxrXwRs6LXRM",
       },
     };
-    var url = `https://api.newscatcherapi.com/v2/latest_headlines?topic=${this.props.topic}&countries=${this.props.country}&page_size=${this.props.size}&page=1`;
+    const url = `https://api.newscatcherapi.com/v2/latest_headlines?topic=${this.props.topic}&countries=${this.props.country}&page_size=${this.props.size}&page=${this.state.page}`;
     const data = await fetch(url, options);
     const parsedData = await data.json();
-    // const total = parsedData.total_hits - parsedData.articles.length;
-    // status: 'ok', total_hits: 10000, page: 5, total_pages: 100, page_size: 100, …}
-    console.log(parsedData.articles);
-    console.log(parsedData.status);
-    if (parsedData.status === "ok") {
-      this.setState({
-        articles: parsedData.articles,
-        loading: false,
-        // totalArticles: total,
-        totalPages: parsedData.total_pages,
-        page: parsedData.page,
-      });
-    } else {
-      this.setState({
-        articles: [],
-        status: parsedData.status,
-        loading: false,
-        totalPages: parsedData.total_pages,
-        page: parsedData.page,
-      });
-    }
+
+    this.setState({
+      articles: parsedData.articles,
+      loading: false,
+      totalPages: parsedData.total_pages,
+    });
+  };
+  componentDidMount = () => {
+    this.data();
   };
 
   clickToNext = async () => {
     this.setState({
-      loading: true,
-    });
-    var options = {
-      method: "GET",
-      headers: {
-        "x-api-key": "SFSG78-MkJhqh_pv5pgSB_aY5pVYHfuUxrXwRs6LXRM",
-      },
-    };
-    var url = `https://api.newscatcherapi.com/v2/latest_headlines?topic=${
-      this.props.topic
-    }&countries=${this.props.country}&page_size=${this.props.size}&page=${
-      this.state.page + 1
-    }`;
-    const data = await fetch(url, options);
-    const parsedData = await data.json();
-
-    this.setState({
-      articles: parsedData.articles,
-      loading: false,
       page: this.state.page + 1,
-      // totalArticles: this.state.totalArticles - parsedData.articles.length,
     });
-
-    // console.log(this.state.totalArticles);
-    console.log(this.state.page);
+    this.data();
   };
+
   clickToPrev = async () => {
     this.setState({
-      loading: true,
-    });
-    var options = {
-      method: "GET",
-      headers: {
-        "x-api-key": "SFSG78-MkJhqh_pv5pgSB_aY5pVYHfuUxrXwRs6LXRM",
-      },
-    };
-    var url = `https://api.newscatcherapi.com/v2/latest_headlines?topic=${
-      this.props.topic
-    }&countries=${this.props.country}&page_size=${this.props.size}&page=${
-      this.state.page - 1
-    }`;
-    const data = await fetch(url, options);
-    const parsedData = await data.json();
-
-    this.setState({
-      articles: parsedData.articles,
-      loading: false,
       page: this.state.page - 1,
-      // totalArticles: this.state.totalArticles + parsedData.articles.length,
     });
-    // console.log(this.state.totalArticles);
+    this.data();
   };
 
   render() {
@@ -118,14 +68,14 @@ export default class News extends Component {
     let loading = this.state.loading;
     return (
       <>
-        {loading === true ? (
+        {loading ? (
           <Spinner />
         ) : (
           <div className="container my-3">
             <h2 className="text-center">Monkey top headline</h2>
             <div className="row">
-              {articles.length === 0 ? (
-                <div className="text-center">{this.state.status}</div>
+              {articles === undefined ? (
+                <div className="text-center">No Data Found</div>
               ) : (
                 articles.map((e) => (
                   <div key={e._id} className="col-md-4">
@@ -152,7 +102,6 @@ export default class News extends Component {
                 &larr; Privious
               </button>
               <button
-                // disabled={this.state.totalArticles <= 0}
                 disabled={this.state.page >= this.state.totalPages}
                 onClick={this.clickToNext}
                 type="button"
